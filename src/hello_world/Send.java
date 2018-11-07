0ce6831bc4156241f570cc556f6a0267ce7834ff
TechnoDexx <oldghost07@mail.ru>
2018-11-07 15:55:39 +0400

    Initial Commit

---
+++ src/hello_world/Send.java
@@ -0,0 +1,26 @@
+package hello_world;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send {
+
+  private final static String QUEUE_NAME = "hello";
+
+  public static void main(String[] argv) throws Exception {
+    ConnectionFactory factory = new ConnectionFactory();
+    factory.setHost("localhost");
+    factory.setUsername("Administrator");
+    factory.setPassword("RVtK28c8r7Td8fmyuChZ");
+    Connection connection = factory.newConnection();
+    Channel channel = connection.createChannel();
+
+    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+    String message = "Hello World!";
+    channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
+    System.out.println(" [x] Sent '" + message + "'");
+
+    channel.close();
+    connection.close();
+  }
+}
